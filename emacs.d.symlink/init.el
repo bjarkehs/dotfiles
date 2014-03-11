a9713caef2d4c35516380de5658847f6c915a88d
bjarkehs <tixusdk@gmail.com>
2014-03-11 09:53:57 +0100

    Renamed from tpanum to bjarkehs

--- emacs.d.symlink/init.el
+++ emacs.d.symlink/init.el
@@ -1,16 +1,16 @@
-;; Tpanum's Emacs configuration
+;; Bjarkehs' Emacs configuration, with lots of inspiration from tpanum.
 
-(defun tpanum/load-init-file (path &optional noerror)
+(defun bjarkehs/load-init-file (path &optional noerror)
   "This loads a file from inside the the .emacs.d directory"
-  (let ((file (file-name-sans-extension 
+  (let ((file (file-name-sans-extension
                (expand-file-name path user-emacs-directory))))
     (load file noerror)))
 
 ;; load configuration settings by type
-(tpanum/load-init-file "tpanum/init/freshen.el")
-(tpanum/load-init-file "tpanum/init/system.el")
-(tpanum/load-init-file "tpanum/init/autoloads.el")
-(tpanum/load-init-file "tpanum/init/packages.el")
-(tpanum/load-init-file "tpanum/init/compile.el")
-(tpanum/load-init-file "tpanum/init/autohooks.el")
-(tpanum/load-init-file "tpanum/init/settings.el")
+(bjarkehs/load-init-file "bjarkehs/init/freshen.el")
+(bjarkehs/load-init-file "bjarkehs/init/system.el")
+(bjarkehs/load-init-file "bjarkehs/init/autoloads.el")
+(bjarkehs/load-init-file "bjarkehs/init/packages.el")
+(bjarkehs/load-init-file "bjarkehs/init/compile.el")
+(bjarkehs/load-init-file "bjarkehs/init/autohooks.el")
+(bjarkehs/load-init-file "bjarkehs/init/settings.el")
